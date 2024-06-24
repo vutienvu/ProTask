57175a209b5e9e67ba6a60ab00e77976483422a4
Tien Vuong Vu <vutienvu@cvut.cz>
2024-06-24 19:43:28 +0200

    Initial migration

---
+++ Migrations/20240624172910_Initial.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProTask.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "programmers",
+                columns: table => new
+                {
+                    programmer_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    age = table.Column<int>(type: "int", nullable: false),
+                    nickname = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_programmers", x => x.programmer_id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "specializations",
+                columns: table => new
+                {
+                    specialization_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    seniority = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_specializations", x => x.specialization_id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "todos",
+                columns: table => new
+                {
+                    todo_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    description = table.Column<string>(type: "longtext", maxLength: 65535, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    priority = table.Column<int>(type: "int", nullable: false),
+                    created_on = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    programmer_id = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_todos", x => x.todo_id);
+                    table.ForeignKey(
+                        name: "fk_todos_programmers_programmer_id",
+                        column: x => x.programmer_id,
+                        principalTable: "programmers",
+                        principalColumn: "programmer_id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "programmer_specialization",
+                columns: table => new
+                {
+                    programmers_programmer_id = table.Column<int>(type: "int", nullable: false),
+                    specializations_specialization_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_programmer_specialization", x => new { x.programmers_programmer_id, x.specializations_specialization_id });
+                    table.ForeignKey(
+                        name: "fk_programmer_specialization_programmers_programmers_programmer",
+                        column: x => x.programmers_programmer_id,
+                        principalTable: "programmers",
+                        principalColumn: "programmer_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_programmer_specialization_specializations_specializations_sp",
+                        column: x => x.specializations_specialization_id,
+                        principalTable: "specializations",
+                        principalColumn: "specialization_id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_programmer_specialization_specializations_specialization_id",
+                table: "programmer_specialization",
+                column: "specializations_specialization_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_todos_programmer_id",
+                table: "todos",
+                column: "programmer_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "programmer_specialization");
+
+            migrationBuilder.DropTable(
+                name: "todos");
+
+            migrationBuilder.DropTable(
+                name: "specializations");
+
+            migrationBuilder.DropTable(
+                name: "programmers");
+        }
+    }
+}
