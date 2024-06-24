be90e6a0db7abda2a2e19a5548db8052f385b12b
Tien Vuong Vu <vutienvu@cvut.cz>
2024-06-24 15:10:34 +0200

    Add required annotation

--- Src/Entity/Todo.cs
+++ Src/Entity/Todo.cs
@@ -8,8 +8,10 @@ public class Todo
     [Key]
     public int TodoId { get; set; }
     
+    [Required]
     [MaxLength(65535)]
     public string Description { get; set; } = String.Empty;
     
+    [Required]
     public PriorityEnum Priority { get; set; }
 }
