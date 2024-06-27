ec0d5ac951a59583176aa32263840ba345ccae7f
Tien Vuong Vu <vutienvu@cvut.cz>
2024-06-27 17:50:17 +0200

    Create add method in todo service

---
+++ Src/Service/TodoService.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using ProTask.Entity;
+using ProTask.Helper;
+using ProTask.Request;
+using ProTask.Response;
+using ProTask.Service.Interface;
+
+namespace ProTask.Service;
+
+public class TodoService : ITodoService
+{
+    private readonly DatabaseContext _dbContext;
+    private readonly DbSet<Todo> _dbSet;
+    
+    public TodoService(DatabaseContext databaseContext)
+    {
+        _dbContext = databaseContext;
+        _dbSet = _dbContext.Set<Todo>();
+    }
+
+    public async Task<TodoResponse> AddAsync(TodoRequest todoRequest)
+    {
+        Todo todo = new Todo();
+        todo.Description = todoRequest.Description;
+        todo.Priority = todoRequest.Priority;
+        var createdTodo = await _dbSet.AddAsync(todo);
+        await _dbContext.SaveChangesAsync();
+
+        TodoResponse todoResponse = new TodoResponse();
+
+        todoResponse.TodoId = createdTodo.Entity.TodoId;
+        todoResponse.Description = createdTodo.Entity.Description;
+        todoResponse.Priority = createdTodo.Entity.Priority;
+        todoResponse.CreatedOn = createdTodo.Entity.CreatedOn;
+        todoResponse.ProgrammerId = createdTodo.Entity.ProgrammerId;
+        
+        return todoResponse;
+    }
+}
